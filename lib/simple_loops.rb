1f9f30f584c6cf20f30480b86082d3a8435adcdf
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-10 19:40:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(string)
     count += 1 
   end 
 end 
+
+def loop_message_n_times(string, n)
+  count = 0 
+  while count <= n do 
+    puts string 
+    count += 1 
+  end 
+end 
